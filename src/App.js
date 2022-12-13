8aeca0cfe7e45974d76c547821b9790cafe02281
Atul Minz <atulminz.jsr@gmail.com>
2022-12-13 22:04:46 +0530

    project done

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/SocialLinks";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Contact />
+
+      <SocialLinks />
+    </div>
+  );
+}
+
+export default App;
