d5f8e81c0cb1cab9c1a63ee61b7d0d7a39576d33
Atul Minz <atulminz.jsr@gmail.com>
2022-12-14 22:06:49 +0530

    Minor Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 
             <div className="flex flex-col justify-center h-full">
                 <h2 className="text-4xl sm:text-7xl font-bold text-white">
-                    I'm a Full Stack Developer.
+                    Hi, I'm a Full Stack Developer.
                 </h2>
                 <p className="text-gray-500 py-4 max-w-md">
                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
